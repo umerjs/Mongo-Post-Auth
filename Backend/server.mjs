f420db2dddd8da737a1ad8bc48eb5cb7aa1b16e9
umerjs <umery101@gmail.com>
2026-08-29 19:44:27 +0500

    cors

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -18,7 +18,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/{*path}", cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
